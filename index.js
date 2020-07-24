50613d30f90783fef7904838fc7e80b1717e10a8
Cory Wang <cory.wang11@gmail.com>
2020-07-24 16:59:36 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+let message = [];
+
+function getDogs(string) {
+    fetch('https://dog.ceo/api/breed/' + string + '/images')
+    .then(response => response.json())
+    .then(responseJson => 
+        displayDogs(responseJson))
+        .catch(error => alert('Warning! Dog API call failed. Try again later.'));
+}
+
+function templateDogHtml(dogUrl){
+    return `<img src="${dogUrl}" class="dog-image">`
+}
+
+function displayDogs(responseJson) {
+    /* Log the JSON object from the response */
+    console.log(responseJson);
+    
+    /* Add each image to the DOM */
+    message = responseJson.message;
+    console.log(message);
+    let type = typeof(message);
+        if (type === 'string') {
+            $('.dog-pics').replaceWith(message);
+            $('dog-pics').removeClass('hidden');
+        }
+        else {
+        $('.dog-pics').replaceWith(templateDogHtml(message[0]));
+        $('dog-pics').removeClass('hidden');
+    }
+}
+
+function formEvent() {
+    $('form').on('submit', function(event){
+        event.preventDefault();
+        let string = $('input').val();
+        string = string.toLowerCase();
+        console.log(string);
+        getDogs(string);
+    });
+}
+
+$(function start() {
+    console.log('Done');
+    formEvent();
+})
